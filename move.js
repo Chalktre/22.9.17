9cb8e94891df5d17cb496f162ca83dfa450de1ca
Chalktre <107284592+Chalktre@users.noreply.github.com>
2022-09-18 11:19:34 +0800

    Add files via upload

---
+++ move.js
@@ -0,0 +1,23 @@
+			function animate(obj, target, callback) {
+				clearInterval(obj.timer);
+				//排他操作
+				obj.timer = setInterval(function() {
+					if (obj.offsetLeft == target) {
+						clearInterval(obj.timer)
+						if(callback) {
+							callback();
+						}
+					}
+					var i = (target - obj.offsetLeft)/10;
+					if(i > 0) {
+						i = Math.ceil(i)
+					}
+					else {
+						i = Math.floor(i)
+					}
+					console.log(i);
+					//如果步长为正，向上取整，否则向下取整
+					obj.style.left = obj.offsetLeft + i + 'px';
+				}, 15)
+				// 封装一个动画函数
+			}
